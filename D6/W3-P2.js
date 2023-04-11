5cfbf1d868b524165d64a0394ac27a66f4369571
simone schioppo <122487693+simoneschioppo98@users.noreply.github.com>
2023-04-11 23:02:27 +0200

    Add files via upload

---
+++ D6/W3-P2.js
@@ -0,0 +1,19 @@
+// document.getElementById("bro").innerText="sTech";
+// //document.body.style.backgroundColor="red";
+// document.getElementById("indirizzo").innerText = "indirizzo fittizo";
+// let newClass = document.querySelectorAll("a");
+// for (let i = 0; i < newClass.length; i++) {
+//     newClass[i].classList.add('amazon-link');
+    
+// }
+// console.log(newClass);
+
+// const classREmover = function (){
+//     let images= document.querySelectorAll("img");
+//     for (let i = 0; i < images.length; i++) {
+//         const oneImg = images[i];
+//         oneImg.classList.toggle("hidden")
+        
+        
+//     }
+// }
